b429820c1d52e801a66e005a34f9e803126a25fc
José Manuel Fuentes Fernández <fuentesfernandezjosemanuel@gmail.com>
2024-06-04 18:49:46 +0200

    creates main character + scroll seleccionMundo sin nav

--- Mazzcha/menu.gd
+++ Mazzcha/menu.gd
@@ -3,11 +3,17 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_on_load_menu()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
+
+
+func _on_load_menu():
+	
 	pass
 
 
