d346541eeef05430721ef39ad96fb1c9d51e3938
JMFF8 <fuentesfernandezjosemanuel@gmail.com>
2024-06-05 19:31:42 +0200

    Create script main Character 1/5 func

--- Mazzcha/personajes/main/characters/main_character.gd
+++ Mazzcha/personajes/main/characters/main_character.gd
@@ -1,2 +1,21 @@
 extends CharacterBody2D
 
+@onready var sprite = $sprite
+@onready var health_bar = $health_bar
+@onready var animation_player = $AnimationPlayer
+
+@export var MAX_HEALTH : float = 10
+
+
+func _ready():
+	animation_player.play("idle_swordman")
+
+
+var health : float = MAX_HEALTH:
+	set(value):
+		health = value
+		_update_progress_bar()
+		
+func _update_progress_bar():
+	health_bar.value = (health/MAX_HEALTH) * 100
+	
